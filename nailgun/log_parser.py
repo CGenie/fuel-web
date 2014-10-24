400758dc887514108149937d2b553ca3ff533998
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-10-24 10:20:42 +0200

    Log parser performance tests
    Change-Id: Id1c59f4832bc7e476d6e99a493e785648e88fdcb

---
+++ nailgun/log_parser.py
@@ -0,0 +1,27 @@
+import re
+import time
+
+from nailgun.api.v1.handlers.logs import read_log
+from nailgun.settings import settings
+
+
+def test_astute_performance():
+    log_file = 'astute.log'
+    log_config = [log for log in settings.LOGS if log['id'] == 'astute'][0]
+
+    max_entries = 2000
+    regexp = re.compile(log_config['regexp'])
+
+    read_log(
+        log_file,
+        log_config=log_config,
+        max_entries=max_entries,
+        regexp=regexp
+    )
+
+
+if __name__ == '__main__':
+    start = time.time()
+    for x in xrange(50):
+        test_astute_performance()
+    print time.time() - start
